76bf377b741164e60abfa7476ca381873b3cdbc1
nabil <nabil@Nazmeen>
2023-08-14 17:35:07 +0530

    first commit

---
+++ sumNum/src/sumNum/sumOfNumber.java
@@ -0,0 +1,16 @@
+package sumNum;
+//sum of numbers by using object
+public class sumOfNumber {
+	int i=10,j=5;
+			int sum;
+	public static void main(String[] args) {
+		System.out.println("the two natural numbers are:");
+		sumOfNumber s=new sumOfNumber();
+		System.out.println(s.sum=s.i+s.j);
+		System.out.println("the sum of two numbers " + s.sum);
+		
+
+	}
+
+
+}
